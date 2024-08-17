01c73e4d50f38f392aa9d4a8d0aa9bd48089b034
MayPhoo-tech-2020 <mayphoowintzayoo@gmail.com>
2024-08-17 16:55:43 +0700

    Add api to create project table and to insert data into project table

---
+++ src/app/api/add-project/route.js
@@ -0,0 +1,34 @@
+'use server';
+
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+
+export async function POST(request) {
+  try {
+    // Parse the incoming JSON data from the request body
+    const { title, description, technologies, languages, contributions } = await request.json();
+
+    // Validate required fields
+    if (!title) throw new Error('Title required');
+    if (!description) throw new Error('Description required');
+    if (!technologies) throw new Error('Technologies required');
+    if (!languages) throw new Error('Languages required');
+    if (!contributions) throw new Error('Contributions required');
+
+    // Insert the new record into the database
+    await sql`
+      INSERT INTO Project (Title, Description, Technologies, Languages, Contributions) 
+      VALUES (${title}, ${description}, ${technologies}, ${languages}, ${contributions});
+    `;
+
+    // Retrieve all records from the database to confirm the insertion
+    const data = await sql`SELECT * FROM Project;`;
+
+    // Convert the data to a JSON string before sending it back in the response
+    return new NextResponse(JSON.stringify({ data }), { status: 200 });
+
+  } catch (error) {
+    // Handle errors and respond with an error message
+    return new NextResponse(JSON.stringify({ error: error.message }), { status: 500 });
+  }
+}
---
+++ src/app/api/create-project-table/route.js
@@ -0,0 +1,24 @@
+//http://localhost:3000/api/create-project-table
+'use server';
+
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+
+export async function POST() {
+  try {
+    const result = await sql`
+      CREATE TABLE Project (
+        id SERIAL PRIMARY KEY,
+        Title varchar(255),
+        Description varchar(300),
+        Technologies varchar (255),
+        Languages varchar(255),
+        Contributions varchar(500)
+
+      );
+    `;
+    return NextResponse.json({ result }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
