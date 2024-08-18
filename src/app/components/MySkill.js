0a011b6df093f6e43ad35e35b9c4bbc78bc73554
MayPhoo-tech-2020 <mayphoowintzayoo@gmail.com>
2024-08-18 21:05:12 +0700

    Add skill

--- src/app/components/MySkill.js
+++ src/app/components/MySkill.js
@@ -11,37 +11,150 @@ return(
             <div className="skills-images">
                 <div className="skill">
                     <img src="/assets/images/java.png" alt="Language 1" className="skill-img" />
-                    <p className="skill-title">Java</p>
+                    <p className="each-title">Java</p>
                 </div>
                 <div className="skill">
                     <img src="/assets/images/java-script.png" alt="Language 2" className="skill-img" />
-                    <p className="skill-title">JavaScript</p>
+                    <p className="each-title">JavaScript</p>
                 </div>
                 <div className="skill">
                     <img src="/assets/images/typescript.png" alt="Language 3" className="skill-img" />
-                    <p className="skill-title">Typescript</p>
+                    <p className="each-title">Typescript</p>
                 </div>
                 <div className="skill">
                     <img src="/assets/images/c-sharp.png" alt="Language 4" className="skill-img" />
-                    <p className="skill-title">C#</p>
+                    <p className="each-title">C#</p>
                 </div>
                 <div className="skill">
                     <img src="/assets/images/python.png" alt="Language 5" className="skill-img" />
-                    <p className="skill-title">Python</p>
+                    <p className="each-title">Python</p>
                 </div>
                 <div className="skill">
                     <img src="/assets/images/php.png" alt="Language 6" className="skill-img" />
-                    <p className="skill-title">PHP</p>
+                    <p className="each-title">PHP</p>
                 </div>
                 <div className="skill">
                     <img src="/assets/images/kotlin.png" alt="Language 7" className="skill-img" />
-                    <p className="skill-title">Kotlin</p>
+                    <p className="each-title">Kotlin</p>
                 </div>
                 <div className="skill">
                     <img src="/assets/images/c-plus.png" alt="Language 8" className="skill-img" />
-                    <p className="skill-title">C++</p>
+                    <p className="each-title">C++</p>
                 </div>
                 
+            </div>
+        </div>
+        <div className="skills-container">
+            <h2 className="skills-title">Query Languages</h2>
+            <hr className="skills-line" />
+            <div className="skills-images">
+                <div className="skill">
+                    <img src="/assets/images/sql.png" alt="Language 1" className="skill-img" />
+                    <p className="each-title">SQL</p>
+                </div>
+                <div className="skill">
+                    <img src="/assets/images/nosql.png" alt="Language 2" className="skill-img" />
+                    <p className="each-title">NoSQL</p>
+                </div>
+            </div>
+           
+        </div>
+        <div className="skills-container">
+        <h2 className="skills-title">Markup Languages</h2>
+        <hr className="skills-line" />
+        <div className="skills-images">
+                <div className="skill">
+                    <img src="/assets/images/html-css.png" alt="Language 1" className="skill-img" />
+                    <p className="each-title">HTML/CSS</p>
+                </div>
+                <div className="skill">
+                    <img src="/assets/images/markdown.png" alt="Language 2" className="skill-img" />
+                    <p className="each-title">Markdown</p>
+                </div>
+            </div>
+        </div>
+        <div className="skills-container">
+        <h2 className="skills-title">Databases</h2>
+        <hr className="skills-line" />
+        <div className="skills-images">
+                <div className="skill">
+                    <img src="/assets/images/html-css.png" alt="Language 1" className="skill-img" />
+                    <p className="each-title">HTML/CSS</p>
+                </div>
+                <div className="skill">
+                    <img src="/assets/images/markdown.png" alt="Language 2" className="skill-img" />
+                    <p className="each-title">Markdown</p>
+                </div>
+            </div>
+        </div>
+        <div className="skills-container">
+        <h2 className="skills-title">Mobile Development Tools</h2>
+        <hr className="skills-line" />
+        <div className="skills-images">
+                <div className="skill">
+                    <img src="/assets/images/html-css.png" alt="Language 1" className="skill-img" />
+                    <p className="each-title">HTML/CSS</p>
+                </div>
+                <div className="skill">
+                    <img src="/assets/images/markdown.png" alt="Language 2" className="skill-img" />
+                    <p className="each-title">Markdown</p>
+                </div>
+            </div>
+        </div>
+        <div className="skills-container">
+        <h2 className="skills-title">Game Development Tool</h2>
+        <hr className="skills-line" />
+        <div className="skills-images">
+                <div className="skill">
+                    <img src="/assets/images/html-css.png" alt="Language 1" className="skill-img" />
+                    <p className="each-title">HTML/CSS</p>
+                </div>
+                <div className="skill">
+                    <img src="/assets/images/markdown.png" alt="Language 2" className="skill-img" />
+                    <p className="each-title">Markdown</p>
+                </div>
+            </div>
+        </div>
+        <div className="skills-container">
+        <h2 className="skills-title">Front-end Development Tools</h2>
+        <hr className="skills-line" />
+        <div className="skills-images">
+                <div className="skill">
+                    <img src="/assets/images/html-css.png" alt="Language 1" className="skill-img" />
+                    <p className="each-title">HTML/CSS</p>
+                </div>
+                <div className="skill">
+                    <img src="/assets/images/markdown.png" alt="Language 2" className="skill-img" />
+                    <p className="each-title">Markdown</p>
+                </div>
+            </div>
+        </div>
+        <div className="skills-container">
+        <h2 className="skills-title">Back-end Development Tools</h2>
+        <hr className="skills-line" />
+        <div className="skills-images">
+                <div className="skill">
+                    <img src="/assets/images/html-css.png" alt="Language 1" className="skill-img" />
+                    <p className="each-title">HTML/CSS</p>
+                </div>
+                <div className="skill">
+                    <img src="/assets/images/markdown.png" alt="Language 2" className="skill-img" />
+                    <p className="each-title">Markdown</p>
+                </div>
+            </div>
+        </div>
+        <div className="skills-container">
+        <h2 className="skills-title">Full-stack Development Tools</h2>
+        <hr className="skills-line" />
+        <div className="skills-images">
+                <div className="skill">
+                    <img src="/assets/images/html-css.png" alt="Language 1" className="skill-img" />
+                    <p className="each-title">HTML/CSS</p>
+                </div>
+                <div className="skill">
+                    <img src="/assets/images/markdown.png" alt="Language 2" className="skill-img" />
+                    <p className="each-title">Markdown</p>
+                </div>
             </div>
         </div>
     </section>
