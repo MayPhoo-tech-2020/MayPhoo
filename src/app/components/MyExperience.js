5b7ef235967c19e624ee3c54857b0e55ce596455
MayPhoo-tech-2020 <mayphoowintzayoo@gmail.com>
2024-08-19 12:20:39 +0700

    Update experience component

--- src/app/components/MyExperience.js
+++ src/app/components/MyExperience.js
@@ -3,29 +3,71 @@ import './MyExperience.css'
 
 const MyExperience = ()=> {
     return(
-        <div className="partfour">
+       
+ <div className="partfour">
               
-                    <section>
-                        <div className="experience-container">
-                            <div className="experience-header">
-                                <img src="/assets/images/mayphoo.jpg" alt="Company Logo" className="experience-logo" />
-                                <h2 className="experience-title">Company Name</h2>
-                            </div>
-                            <hr className="experience-line" />
-                            <div className='experience-details'>
-                                <p className='experience-description'>
-                                    I have gained extensive experience in various areas:
-                                </p>
-                                <ul className='experience-list'>
-                                    <li>Developing and maintaining web applications.</li>
-                                    <li>Collaborating with cross-functional teams to deliver high-quality software.</li>
-                                    <li>Implementing new features and optimizing performance.</li>
-                                    <li>Providing technical support and troubleshooting issues.</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </section>
-                </div>
+              <section>
+              <div className="experience-container">
+                      <div className="experience-header">
+                          <img src="/assets/images/lithan.jpeg" alt="Company Logo" className="experience-logo" />
+                          <h2 className="experience-title">Lithan, On-Campus Mentor(Software Engineering)</h2>
+                      </div>
+                      <hr className="experience-line" />
+                      <div className='experience-details'>
+                          <p className='experience-description'>
+                              I have gained extensive experience in various areas:
+                          </p>
+                          <ul className='experience-list'>
+                              <li>Developing and maintaining web applications.</li>
+                              <li>Collaborating with cross-functional teams to deliver high-quality software.</li>
+                              <li>Implementing new features and optimizing performance.</li>
+                              <li>Providing technical support and troubleshooting issues.</li>
+                          </ul>
+                      </div>
+                  </div>              
+                  <div className="experience-container">
+                      <div className="experience-header">
+                          <img src="/assets/images/360ed.png" alt="Company Logo" className="experience-logo" />
+                          <h2 className="experience-title">360ed, Application Developer</h2>
+                      </div>
+                      <hr className="experience-line" />
+                      <div className='experience-details'>
+                          <p className='experience-description'>
+                              I have gained extensive experience in various areas:
+                          </p>
+                          <ul className='experience-list'>
+                              <li>Developing and maintaining web applications.</li>
+                              <li>Collaborating with cross-functional teams to deliver high-quality software.</li>
+                              <li>Implementing new features and optimizing performance.</li>
+                              <li>Providing technical support and troubleshooting issues.</li>
+                          </ul>
+                      </div>
+                  </div>    
+                  <div className="experience-container">
+                      <div className="experience-header">
+                          <img src="/assets/images/360ed.png" alt="Company Logo" className="experience-logo" />
+                          <h2 className="experience-title">360ed, Android Developer(Internship)</h2>
+                      </div>
+                      <hr className="experience-line" />
+                      <div className='experience-details'>
+                          <p className='experience-description'>
+                              I have gained extensive experience in various areas:
+                          </p>
+                          <ul className='experience-list'>
+                              <li>Developing and maintaining web applications.</li>
+                              <li>Collaborating with cross-functional teams to deliver high-quality software.</li>
+                              <li>Implementing new features and optimizing performance.</li>
+                              <li>Providing technical support and troubleshooting issues.</li>
+                          </ul>
+                      </div>
+                  </div>                        
+                   
+              </section>
+              
+          </div>
+        
+       
+       
 
     );
 };
