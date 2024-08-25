6ace2b539db53c9873354d8fc90d5c30ffe899e0
MayPhoo-tech-2020 <mayphoowintzayoo@gmail.com>
2024-08-25 22:06:57 +0700

    remove , and update with &apos;

--- src/app/components/Intro.js
+++ src/app/components/Intro.js
@@ -10,7 +10,7 @@ return(
         <p className='intro'>Hello, I&apos;m</p>
         <h2>May Phoo Wint Zay Oo</h2>
         <div className='intro-txt'>
-            <p>Hello, I'm a Full-Stack Developer with a passion for creating seamless and efficient web applications. I enjoy tackling challenges that require both front-end creativity and back-end precision, always striving to deliver robust and scalable solutions.</p>
+            <p>Hello, I&apos;m a Full-Stack Developer with a passion for creating seamless and efficient web applications. I enjoy tackling challenges that require both front-end creativity and back-end precision, always striving to deliver robust and scalable solutions.</p>
         </div>
         <div className="buttons-container">
             <a href="/assets/document/mayphoowintzayooresume.pdf" download>
